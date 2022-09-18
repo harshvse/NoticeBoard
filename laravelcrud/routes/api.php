f04f815dd99e48effaa4e0caa351c46340c27748
harshvse <harshvse@gmail.com>
2022-09-18 23:00:38 +0530

    added the ability to add new post using REST API

--- laravelcrud/routes/api.php
+++ laravelcrud/routes/api.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::Post('/add-post',[PostController::class, 'store'] );
+Route::Post('/addPost',[PostController::class, 'store'] );
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
