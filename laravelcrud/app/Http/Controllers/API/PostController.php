7f48209d2201aab9b4116401376b9abda884e9d1
harshvse <harshvse@gmail.com>
2022-09-18 18:50:42 +0530

    added Create method in post controller

--- laravelcrud/app/Http/Controllers/API/PostController.php
+++ laravelcrud/app/Http/Controllers/API/PostController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use APP\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function store(Request $request){
+        $post = new Post;
+        $post -> title = $request -> input('title');
+        $post -> title = $request -> input('type');
+        $post -> title = $request -> input('desc');
+        $post -> save();
+
+        return respose() -> json([
+            'status'=> 200,
+            'message' => 'Post Stored Successfully'
+        ]);
+    }
 }
