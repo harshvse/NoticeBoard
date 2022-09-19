72b4eb9ae10ed1351d673392802a26863b0657f9
harshvse <harshvse@gmail.com>
2022-09-19 13:05:08 +0530

    added the ability to fetch first n posts based on a count variable

--- laravelcrud/app/Http/Controllers/API/PostController.php
+++ laravelcrud/app/Http/Controllers/API/PostController.php
@@ -8,6 +8,16 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
+    public function index(Request $request){
+        $count = $request -> input('count');
+        $posts = Post::orderBy('updated_at', 'asc') ->take($count)->get(); ;
+        return response() -> json([
+            'status'=> 200,
+            'posts' => $posts,
+            'message' => 'Students Fetched Successfully'
+        ]);
+    }
+
     public function store(Request $request){
         $post = new Post;
         $post -> title = $request -> input('title');
--- laravelcrud/routes/api.php
+++ laravelcrud/routes/api.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::Post('/addPost',[PostController::class, 'store'] );
+Route::post('/addPost',[PostController::class, 'store'] );
 
-
+Route::post('/posts', [PostController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
